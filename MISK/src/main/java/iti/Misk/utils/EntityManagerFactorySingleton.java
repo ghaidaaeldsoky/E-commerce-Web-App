b75d8bff105e97149d32ed966f5c75c0dfc07c13
Ahmad Taher <ahmad.t.yassen@gmail.com>
2025-04-10 15:34:38 +0200

    counter 100100

--- MISK/src/main/java/iti/Misk/utils/EntityManagerFactorySingleton.java
+++ MISK/src/main/java/iti/Misk/utils/EntityManagerFactorySingleton.java
@@ -1,16 +1,37 @@
 package iti.Misk.utils;
 
+import com.zaxxer.hikari.HikariDataSource;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class EntityManagerFactorySingleton {
 
-    private static final EntityManagerFactory entityManagerFactory =
-            Persistence.createEntityManagerFactory("main");
+    private static volatile EntityManagerFactory entityManagerFactory;
+
 
    private EntityManagerFactorySingleton() {}
 
     public static EntityManagerFactory getEntityManagerFactory() {
+
+       if(entityManagerFactory == null) {
+            synchronized (EntityManagerFactorySingleton.class) {
+                if (entityManagerFactory == null) {
+
+
+                    entityManagerFactory = Persistence.createEntityManagerFactory("main");
+                }
+            }
+        }
         return entityManagerFactory;
+    }
+
+
+    public static void close() {
+        if (entityManagerFactory != null) {
+            entityManagerFactory.close();
+        }
     }
 }
